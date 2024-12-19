43f7a78afface6c4e03a7d7e2c44f47ea150362c
Gustavo Rivera <griverab@unal.edu.co>
2024-12-18 23:47:24 -0500

    install cors

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 import express from "express";
+import cors from "cors";
 import storeRoutes from './routes/stores.js';
 
 const PORT = 8800;
 const app = express();
 
+// Configuración básica de CORS
+app.use(cors());
+
+// permitir solo ciertos orígenes
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  credentials: true, 
+}));
+
 
 app.use("/search-ms/stores", storeRoutes);
+
+app.options("/", (req, res) => {
+    //res.setHeader("Access-Control-Allow-Origin", FRONTEND_URL);
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.sendStatus(204);
+  });
+  
 
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
