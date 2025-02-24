2405111057b806c383ac7361485df9e055bc3ce4
Gustavo Rivera <griverab@unal.edu.co>
2025-02-24 00:48:27 -0500

    config var entorno

--- connect-elastic.js
+++ connect-elastic.js
@@ -1,7 +1,10 @@
 import { Client } from "@elastic/elasticsearch";
+import dotenv from "dotenv";
+dotenv.config();
 
+const URL_ELASTICSEARCH = process.env.URL_ELASTICSEARCH;
 const elasticClient = new Client({
-  node: process.env.URL_ELASTICSEARCH, // URL del servicio en Docker
+  node: URL_ELASTICSEARCH, // URL del servicio en Docker
 });
 
 export default elasticClient;
