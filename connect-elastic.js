76f8c1667685ff1f3bf2eabb579260e2f24bc8fd
Gustavo Rivera <griverab@unal.edu.co>
2025-03-01 22:44:44 -0500

    add elasticsearch credentials

--- connect-elastic.js
+++ connect-elastic.js
@@ -3,8 +3,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const URL_ELASTICSEARCH = process.env.URL_ELASTICSEARCH;
+const API_KEY = process.env.API_KEY_ELASTICSEARCH;
+// const elasticClient = new Client({
+//   node: URL_ELASTICSEARCH, // URL del servicio en Docker
+// });
+
 const elasticClient = new Client({
-  node: URL_ELASTICSEARCH, // URL del servicio en Docker
+  node: URL_ELASTICSEARCH,
+  auth: {
+    apiKey: API_KEY,
+  },
 });
 
 export default elasticClient;
